8990eabb452639a75ee67d1928e551c76c4ffa67
David Rajcher (Codaisseur) <davidrajchercodaisseur@Davids-MBP.codaisseur.local>
2022-02-24 14:25:52 +0100

    updated migrations

---
+++ migrations/20220224131936-create-todo.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      task: {
+        type: Sequelize.STRING
+      },
+      listId: {
+        type: Sequelize.INTEGER
+      },
+      deadline: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Todos');
+  }
+};
